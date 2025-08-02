efc92300cd5d3695f81f819c58d74a9e87437730
vinivosh <36175157+vinivosh@users.noreply.github.com>
2025-08-02 18:01:53 -0300

    🔧 Separate projects into microservices + an SDK and init them all

---
+++ nuvie-ingestor/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from nuvie-ingestor!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ nuvie-sdk/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from nuvie-sdk!")
+
+
+if __name__ == "__main__":
+    main()
