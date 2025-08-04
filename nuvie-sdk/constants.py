ea9c36382589aedf0384522d0c403b9623ee4c4a
vinivosh <36175157+vinivosh@users.noreply.github.com>
2025-08-04 03:17:22 -0300

    💬 sdk - Add various other constants

--- nuvie-sdk/constants.py
+++ nuvie-sdk/constants.py
@@ -1,5 +1,33 @@
 """Contains constants used throughout Nuvie SDK."""
 
+import os
+
+import dotenv
+
+# Loading ".env" file
+dotenv.load_dotenv()
+
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
+
+
+# * ###########################################################################
+# * DB credentials
+# * ###########################################################################
+
+POSTGRES_SERVER = os.getenv("POSTGRES_SERVER", "postgres")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+POSTGRES_DB = os.getenv("POSTGRES_DB", "payfy-pkmn")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "")
+
+
+def get_postgres_uri():
+    """Returns the PostgreSQL URI for SQLAlchemy."""
+
+    return f"postgresql+psycopg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+
 # * ###########################################################################
 # * DB table names
 # * ###########################################################################
