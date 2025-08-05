2ef1b7eb0c65ad5771b45b8bbe9703aa9b357feb
vinivosh <36175157+vinivosh@users.noreply.github.com>
2025-08-05 02:18:53 -0300

    🐛 backend - Start actually creating the first superuser!

--- nuvie-backend/src/db.py
+++ nuvie-backend/src/db.py
@@ -4,6 +4,7 @@ from logger import log
 import constants as c
 
 from nuvie_sdk.models import User, UserCreate
+from nuvie_sdk.use_cases import user_use_case
 
 
 engine = create_engine(str(c.get_postgres_uri()))
@@ -32,7 +33,6 @@ def init_db(session: Session) -> None:
         )
         log.debug(f"New user data: {new_user}")
 
-        # TODO: Implement user creation
-        # user_use_case.create_user(session=session, user_create=new_user)
+        user_use_case.create_user(session=session, user_create=new_user)
 
         log.info("First superuser created with success!")
