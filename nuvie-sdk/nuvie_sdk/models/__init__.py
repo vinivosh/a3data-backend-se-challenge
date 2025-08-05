0577427e749f21781b822acd8ce97a2571336811
vinivosh <36175157+vinivosh@users.noreply.github.com>
2025-08-05 03:57:41 -0300

    🗑️ sdk - Comment out unused function

--- nuvie-sdk/nuvie_sdk/models/__init__.py
+++ nuvie-sdk/nuvie_sdk/models/__init__.py
@@ -1,8 +1,9 @@
 """This file contains the models used in the Nuvie SDK."""
 
 from .user import *
+from .patient import *
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import SQLModel  # , Field
 
 
 class Message(SQLModel):
@@ -24,8 +25,9 @@ class TokenPayload(SQLModel):
     sub: int | None = None
 
 
-class NewPassword(SQLModel):
-    """Model for changing a user's password."""
+# TODO: Am I not using this anywhere?
+# class NewPassword(SQLModel):
+#     """Model for changing a user's password."""
 
-    token: str
-    new_password: str = Field(min_length=8, max_length=128)
+#     token: str
+#     new_password: str = Field(min_length=8, max_length=128)
