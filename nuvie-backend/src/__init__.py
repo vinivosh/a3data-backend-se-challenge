b2e4e34a816780530a85822417dbbba9d8ee7423
vinivosh <36175157+vinivosh@users.noreply.github.com>
2025-08-04 22:01:37 -0300

    🚧 backend - Start importing nuvie_sdk!

--- nuvie-backend/src/__init__.py
+++ nuvie-backend/src/__init__.py
@@ -1,15 +1,16 @@
-from typing import Union
-
 from fastapi import FastAPI
+
+import nuvie_sdk as nuv
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
+    print("Nuvie Backend is running! Imported nuvie_sdk:", nuv)
     return {"msg": "Olá, mundo! Bem-vindo ao Nuvie Backend!"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
